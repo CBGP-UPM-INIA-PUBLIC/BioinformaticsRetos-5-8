0ae1fb2795c144645d314c6abd241d68d42077e0
Mark Wilkinson <markw@illuminae.com>
2022-11-23 14:26:45 +0100

    moved the Module into this course git. updatd docuemntation

---
+++ Lectures/mdw_objects/patient.rb
@@ -0,0 +1,12 @@
+module Mdw_objects
+	class Patient
+
+	    attr_accessor :name  # create an "attribute accessor" (read and write) for "name"
+  
+ 	   def initialize (name: "Some Person") # get a name from the "new" call, or set a default
+      		@name = name  
+ 	   end
+
+	  # details about the class go here...
+	end
+end
